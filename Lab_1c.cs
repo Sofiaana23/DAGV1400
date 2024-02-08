2e37b508d74885c8b82dac61e21b0c3903931c26
sofiaana23 <anasofia.eyre@gmail.com>
2024-02-07 22:43:12 -0700

    Oops
    Forgot to save comments

--- Lab_1c.cs
+++ Lab_1c.cs
@@ -6,11 +6,12 @@ namespace MyApplication
   {
         static void Main(string[] args)
     {
+        //Prompt user to enter a positive number
         Console.WriteLine ("Please enter a positive integer");
         string input = Console.ReadLine();
         int pyramidNumber = Int32.Parse(input);
         Console.WriteLine("------------------");
-
+        //Use of loops
         for (int i = 1; i <= pyramidNumber; i++)//row
         {
             for (int h = 1; h <= i; h++) // collumn
